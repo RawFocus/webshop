b2ed4c10c7715cb9394cfb36a68fa65cf45b527a
yvovonBerg <pobox99991+github@protonmail.com>
2023-05-17 00:11:26 +0200

    test

--- src/Http/Controllers/CheckoutController.php
+++ src/Http/Controllers/CheckoutController.php
@@ -13,20 +13,20 @@ class CheckoutController extends Controller
 {
     public function postCheckout(CheckoutRequest $request)
     {
-        try
-        {
+        // try
+        // {
             return response()->json([
                 "status" => "success",
                 "url" => Payments::checkoutFromRequest($request)
             ]);
-        }
-        catch (Exception $e)
-        {
-            Log::error($e);
-            return response()->json([
-                "status" => "error",
-                "error" => __("webshop::validation.general_error")
-            ]);
-        }
+        // }
+        // catch (Exception $e)
+        // {
+        //     Log::error($e);
+        //     return response()->json([
+        //         "status" => "error",
+        //         "error" => __("webshop::validation.general_error")
+        //     ]);
+        // }
     }
 }
