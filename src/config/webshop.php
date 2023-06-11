b6bd46aa69e8770cff0e60501d57ec5447e386bc
yvovonBerg <pobox99991+github@protonmail.com>
2023-06-11 15:36:32 +0200

    links in config file

--- src/config/webshop.php
+++ src/config/webshop.php
@@ -11,19 +11,21 @@ return [
 
         // Endpoints stripe should use for success and cancel redirects
         "urls" => [
-            "success" => env("STRIPE_SUCCESS_URL", ""),
-            "cancel" => env("STRIPE_CANCEL_URL", ""),
+            "success" => env("STRIPE_SUCCESS_URL", "https://staging.klimbuddies.nl/"),
+            "cancel" => env("STRIPE_CANCEL_URL", "https://staging.klimbuddies.nl/"),
         ],
 
-        // Tax rates to use; see: [link here] for more information
+        // Tax rates to use; see: https://github.com/RawFocus/webshop and https://stripe.com/docs/invoicing/taxes/tax-rates
         "tax_rates" => [
+            // This ID corresponds to a 21% tax rate. These settings can be managed here: https://dashboard.stripe.com/test/tax-rates
             "high" => env("STRIPE_HIGH_BTW", "txr_1M3fr0KnEV82zsZasVlNH3g8")
         ],
 
-        // Payment methods to use; see: [link here] for more information
+        // Payment methods to use; see: https://github.com/RawFocus/webshop and https://stripe.com/docs/invoicing/payment-methods
         "payment_method_types" => [explode(",", env("STRIPE_PAYMENT_METHODS", "ideal"))],
 
-        // Enable webhook signature validation; see: [link here] for more information
+        // Enable webhook signature validation; see: https://github.com/RawFocus/webshop for more information
+        // see also https://stripe.com/docs/webhooks/signatures
         "enable_webhook_signature_validation" => env("STRIPE_ENABLE_WEBHOOK_SIGNATURE_VALIDATION", false),
 
     ],
@@ -38,13 +40,13 @@ return [
         "middleware" => [
 
             // Authenticated only route middlewares
-            "auth" => ["auth:sanctum"],
+            "auth" => ["auth:sanctum", "registration"], // TODO: remove registration from package to make the package more agnostic
 
             // Product route middlewares
-            "products" => [],
+            "products" => ["is_admin"], // TODO: remove is_admin from package to make the package more agnostic
 
             // Order route middlewares
-            "orders" => [],
+            "orders" => ["is_admin"], // TODO: remove is_admin from package to make the package more agnostic
 
             // Checkout route middelewares
             "checkout" => [],
